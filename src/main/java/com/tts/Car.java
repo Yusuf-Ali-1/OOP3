ce2c015a4357a8affcae1c49f9c3a35751fb54af
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-21 08:41:00 -0400

    2nd commit

--- src/main/java/com/tts/Car.java
+++ src/main/java/com/tts/Car.java
@@ -1,4 +1,24 @@
 package com.tts;
 
-public class Car {
+public class Car implements Driveable{
+
+    public int speed;
+    public int gear;
+
+
+
+    @Override
+    public void changeGear(int a) {
+        System.out.println("This is changing the gear");
+    }
+
+    @Override
+    public void accelerate(int a) {
+
+    }
+
+    @Override
+    public void brake(int a) {
+
+    }
 }
