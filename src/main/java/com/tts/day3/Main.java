3586d1853526830eb7309ca0bd5e603baa798c08
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-21 11:44:16 -0400

    2nd commit

--- src/main/java/com/tts/day3/Main.java
+++ src/main/java/com/tts/day3/Main.java
@@ -8,6 +8,12 @@ import com.tts.day3.dealership.LightTruck;
 import com.tts.day3.dealership.VehicleClassification;
 import com.tts.day3.enumeration.Day;
 import com.tts.day3.enumeration.EnumTest;
+import com.tts.day3.factory.Department;
+import com.tts.day3.factory.DepartmentFactory;
+import com.tts.day3.factory.DepartmentNames;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // a class is a blueprint to create an object
 // an object is going to be an instance of class
@@ -72,6 +78,25 @@ public class Main {
         // enum constructors have private access by default
         // This method below is illegal
         //VehicleClassification vehicleClassification = new VehicleClassification();
+
+        System.out.println("===== Department =====");
+
+        Department serviceDepartment = DepartmentFactory.
+                CreateDepartment(DepartmentNames.SERVICE);
+
+
+        Department financialDepartment = DepartmentFactory.
+                CreateDepartment(DepartmentNames.FINANCE);
+
+        Department salesDepartment = DepartmentFactory.
+                CreateDepartment(DepartmentNames.SALES);
+
+        List<Department> depts = new ArrayList<>();
+        depts.add(serviceDepartment);
+        depts.add(financialDepartment);
+        depts.add(salesDepartment);
+
+        System.out.println("The elements of depts are: " + depts);
 
 
 
