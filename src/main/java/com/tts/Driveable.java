7a5c88a11c0b1f18d474cdeeb3b20499760896b9
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-21 08:26:09 -0400

    1st commit

---
+++ src/main/java/com/tts/Driveable.java
@@ -0,0 +1,8 @@
+package com.tts;
+
+public interface Driveable {
+    // this is my driveable interface
+    // for most if not all vehicles
+    // anything that can driven will
+    // implement this interface
+}
