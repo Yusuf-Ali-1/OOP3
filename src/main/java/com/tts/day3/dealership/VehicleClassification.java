1fab66fd803c2055c6072923094b083542574d3a
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-21 11:19:34 -0400

    2nd commit

--- src/main/java/com/tts/day3/dealership/VehicleClassification.java
+++ src/main/java/com/tts/day3/dealership/VehicleClassification.java
@@ -1,10 +1,32 @@
 package com.tts.day3.dealership;
 
 public enum VehicleClassification {
-    NEW,
-    USED,
-    OFF_LEASE,
-    SHUTTLE,
-    LOANER,
-    PARTS_RUNNER
+
+    NEW(true, true),
+    USED(false, true),
+    OFF_LEASE(true, true),
+    SHUTTLE(true, false),
+    LOANER(false, false),
+    PARTS_RUNNER(true, true);
+
+    private boolean warranty;
+    private boolean freeService;
+
+
+    public boolean WarrantyOffered()
+    {
+        return warranty;
+    }
+
+    public boolean FreeServiceOffered()
+    {
+        return freeService;
+    }
+
+    // constructor must be private or have no modifier
+     VehicleClassification(boolean warranty, boolean freeService)
+    {
+        this.warranty = warranty;
+        this.freeService = freeService;
+    }
 }
