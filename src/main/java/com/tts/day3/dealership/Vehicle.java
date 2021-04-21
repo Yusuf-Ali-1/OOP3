5d4f6e7be664ee997f53027682d64a0afe32aba9
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-21 10:59:47 -0400

    2nd commit

--- src/main/java/com/tts/day3/dealership/Vehicle.java
+++ src/main/java/com/tts/day3/dealership/Vehicle.java
@@ -18,5 +18,33 @@ public class Vehicle {
         //    // these behaviors can be described as methods
     }
 
-    public
+
+    public void printVehicleType()
+    {
+        String type = "NOT SET";
+        switch (this.vehicleClass)
+        {
+            case NEW:
+                type="New Car";
+                break;
+            case USED:
+                type = "User Car";
+                break;
+            case OFF_LEASE:
+                type = "Leased Car";
+                break;
+            case SHUTTLE:
+                type = "Dealer Shuttle";
+                break;
+            case LOANER:
+                type = "Dealer Service Loaner";
+                break;
+            case PARTS_RUNNER:
+                type = "Parts runner";
+                break;
+            default:
+                break;
+        }
+        System.out.println("This car type is: " + type);
+    }
 }
