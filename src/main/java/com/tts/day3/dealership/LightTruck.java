91891385b7ab412ffbfe7f4f94e68048c13d7d66
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-21 10:58:10 -0400

    2nd commit

--- src/main/java/com/tts/day3/dealership/LightTruck.java
+++ src/main/java/com/tts/day3/dealership/LightTruck.java
@@ -2,45 +2,44 @@ package com.tts.day3.dealership;
 
 public class LightTruck {
 
-    private boolean is4WD;
+    // keep in mind that whenever we create classes
+    // we can use them as types
+    public class TransferCase {
 
-    public LightTruck() {
-
-
-    }
-
-    public class TransferCase{
         private int numGears;
 
-        public TransferCase(){
+        public void setNumGears(int numGears) {
+            this.numGears = numGears;
+        }
+
+        public TransferCase() {
             this.numGears = 4;
         }
 
-        public TransferCase(int gears){
+        public TransferCase(int gears) {
             this.numGears = gears;
         }
 
-        public int getNumGears(){
+        public int getNumGears() {
             return numGears;
         }
+
+
     }
 
-    // This lightTruck has a perperty/field
-    // that is of type transfer case
+    // this lightTruck has a property/field
+    // that is of type Transfer case
     public TransferCase transferCase;
+    public boolean is4WD;
     public Integer amountOfWheels;
 
-    public LightTruck(boolean is4WD){
-        this.is4WD = is4WD;
+    public LightTruck(boolean is4WD) {
 
-        if(this.is4WD){
+        this.is4WD = is4WD;
+        if (this.is4WD) {
             this.transferCase = new TransferCase();
         }
-    }
 
-    public LightTruck(boolean is4WD, TransferCase transferCase){
-        this.is4WD = is4WD;
-        this.transferCase = transferCase;
     }
 
 }
---
+++ src/main/java/com/tts/day3/dealership/Vehicle.java
@@ -0,0 +1,22 @@
+package com.tts.day3.dealership;
+
+public class Vehicle {
+
+    private VehicleClassification vehicleClassification;
+
+    public VehicleClassification getVehicleClassification() {
+        return vehicleClassification;
+    }
+
+    public void setVehicleClassification(VehicleClassification vehicleClassification) {
+        this.vehicleClassification = vehicleClassification;
+
+        //    public int wheels = 4;
+        //
+        //    // a vehicle can drive
+        //    // start its engine
+        //    // these behaviors can be described as methods
+    }
+
+    public
+}
