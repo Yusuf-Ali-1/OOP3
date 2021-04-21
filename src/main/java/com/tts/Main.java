bf984f224f53c3f2d725be5b6a567a72278a56f9
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-21 08:15:54 -0400

    1st commit

--- src/main/java/com/tts/Main.java
+++ src/main/java/com/tts/Main.java
@@ -1,4 +1,10 @@
 package com.tts;
 
+// a class is a blueprint to create an object
+// an object is going to be an instance of class
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
 }
