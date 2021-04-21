d19314c5c8027ca25f823972442e1114446405f5
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-21 08:37:57 -0400

    2nd commit

--- src/main/java/com/tts/Driveable.java
+++ src/main/java/com/tts/Driveable.java
@@ -9,6 +9,8 @@ public interface Driveable {
 
     // these methods do not have a body
     // methods in interfaces do not have implementations
+
+    //these methods are referred to as "abstract"
     void changeGear(int a);
     void accelerate(int a);
     void brake(int a);
